3c807b922d433aa778429d2b54f1fee52307bc8b
Joshhortt <joshhortt@yahoo.com>
2021-01-23 14:00:51 +0000

    First Commit of HelloWorld

---
+++ 0.Csharp-FirstApp/HelloWorldApplication/ConsoleUI/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How are you?");
+
+            Console.WriteLine("Isn't this cool?");
+          
+            Console.WriteLine("How are you?");
+
+            Console.WriteLine("This is safer?");
+
+            Console.ReadLine();
+
+
+        }
+    }
+}

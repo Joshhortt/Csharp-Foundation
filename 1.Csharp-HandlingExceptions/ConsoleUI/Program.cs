557a61dade4a524cfb15c33a1e6c6feb95f4d291
Joshhortt <joshhortt@yahoo.com>
2021-01-24 18:04:03 +0000

    Added solution' file. Testing 2 more examples of Try catch and finally exceptions.

--- 1.Csharp-HandlingExceptions/ConsoleUI/Program.cs
+++ 1.Csharp-HandlingExceptions/ConsoleUI/Program.cs
@@ -1,9 +1,11 @@
-﻿using ExceptionsLibrary;
+﻿
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
 
 namespace ConsoleUI
 {
@@ -11,6 +13,7 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            /* // ############################ 1. Try catch ####################################
             DemoCode demo = new DemoCode();
 
             try
@@ -36,6 +39,52 @@ namespace ConsoleUI
             }
 
             Console.ReadLine();
+            */
+
+
+
+            // ############################ 2. Try catch ####################################
+            /* try {
+
+                 StreamReader file = new StreamReader("file.txt"); // 1. Block of code. We think it's going to give us an error
+
+                     // ..... More code errors can occur here as well
+
+             } catch (FileNotFoundException)  {// just for the file not found
+               Debug.WriteLine("File name not found!");  // In this case after starting thsi aplication it will throw an messagae 'File name not found?
+
+             } catch (Exception) {
+                // throw;   // 2. Block of code is useful for only when the error occurs. Ex: If the file above doesn't exist then it will throw an errow here
+               Debug.WriteLine("An Error occured!"); // Error message examplo
+             }
+            */
+
+
+
+            // ############################ 3. Try catch finally ####################################
+
+            StreamReader file = null;
+            try
+            {
+
+                file = new StreamReader("file.txt"); // 1. Block of code. We think it's going to give us an error
+
+                // ..... More code errors can occur here as well
+
+            }
+            catch (FileNotFoundException)
+            {// just for the file not found
+                Debug.WriteLine("File name not found!");  // In this case after starting thsi aplication it will throw an messagae 'File name not found?
+
+            }
+            catch (Exception)
+            {
+                // throw;   // 2. Block of code is useful for only when the error occurs. Ex: If the file above doesn't exist then it will throw an errow here
+                Debug.WriteLine("An Error occured!"); // Error message examplo
+            } finally
+			{
+                if(file != null)   file.Dispose();   // 3. Liberate the file when you don't want it anymore open 
+			}
         }
     }
 }

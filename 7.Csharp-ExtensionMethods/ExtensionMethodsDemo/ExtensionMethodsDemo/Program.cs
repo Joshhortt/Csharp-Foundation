58e204ef8e8369fad40a5b7d868b79dcc238c6e1
Joshhortt <joshhortt@yahoo.com>
2021-01-24 18:49:02 +0000

    First Commmit of Understanding Extension Methods.

---
+++ 7.Csharp-ExtensionMethods/ExtensionMethodsDemo/ExtensionMethodsDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+namespace ExtensionMethodsDemo
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string name = "John";
+			Debug.WriteLine(name.RepeatTwice());
+
+			string surName = "Snow";
+			surName.RepeatTwice();
+			
+		}
+	}
+	static class MyExtensions
+	{
+		public static string RepeatTwice(this string s)   // (this) is related to data type string in this case
+		{
+			return s + s;  // It´s going to print out John twice -- >> JohnJohn
+		}
+			}
+}

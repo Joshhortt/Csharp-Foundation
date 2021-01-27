770b6babec913dfdec9ee7ecc86eb670503db3cf
Joshhortt <joshhortt@yahoo.com>
2021-01-27 15:30:32 +0000

    Printing with double zero decimal in front of Random generated numbers

--- 8.Csharp-RandomNumbers/RandomNumbersDemo/RandomNumbersDemo/Program.cs
+++ 8.Csharp-RandomNumbers/RandomNumbersDemo/RandomNumbersDemo/Program.cs
@@ -17,39 +17,41 @@ namespace RandomNumbersDemo
 				
 				{
 				// 2. Comment -- >> Console.WriteLine(random.Next(5, 8));  
-				SimpleMethod(random);
+				Console.WriteLine(random.NextDouble());  // 2. Adding Double to next
+				// SimpleMethod(random);    3. Cooment
 			}
 
 			Console.ReadLine();
 		}
-		private static void SimpleMethod(Random random)  // 3. Create private static method
+		private static void SimpleMethod(Random random) 
 		{
 			Console.WriteLine(random.Next());
 		}
 	}
 }
 
-/*OUTPUT FIRST ATTEMPT( 10 whole numbers)
-200294297
-9625400
-180075971
-2093405899
-1050823125
-218307568
-842187541
-1731040482
-1093104048
-252448969
+/*OUTPUT FIRST ATTEMPT( 10 double numbers)
+0,620261125555384
+0,626037095033581
+0,192714978099202
+0,658925515440724
+0,84009393576537
+0,749256377922956
+0,03695912195228
+0,0407107477265926
+0,0368781406604117
+0,440451943986328
  
-/*OUTPUT SECOND ATTEMPT( 10 whole numbers)
-1639188983
-1059563538
-544986068
-751871878
-1311898542
-498438722
-304793607
-952429039
-718958311
-173445454
+/*OUTPUT SECOND ATTEMPT( 10 double numbers)
+0,844076678549907
+0,111769080679756
+0,738599690021295
+0,362422578205551
+0,413602239644901
+0,497160597470198
+0,0484613171072962
+0,754646947027485
+0,374913536652417
+0,286118351987618
+
  */

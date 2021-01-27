5d9bac91fe66941e9735157371c6a5fac3474d7c
Joshhortt <joshhortt@yahoo.com>
2021-01-27 15:24:22 +0000

    Printing 'RandomNumbers' but with na better way of doing things.

--- 8.Csharp-RandomNumbers/RandomNumbersDemo/RandomNumbersDemo/Program.cs
+++ 8.Csharp-RandomNumbers/RandomNumbersDemo/RandomNumbersDemo/Program.cs
@@ -10,40 +10,46 @@ namespace RandomNumbersDemo
 	{
 		static void Main(string[] args)
 		{
-			// 1. Printing out random numbers less than 8 including 5 with passing in a seed number of 1
-			Random random = new Random(1);  // 2.  with passing in a seed number of 1
+			// 1. Using Random well ...
+			Random random = new Random();
+
 			for (int i = 0; i < 10; i++)
 				
 				{
-				
-				Console.WriteLine(random.Next(5, 8));  
+				// 2. Comment -- >> Console.WriteLine(random.Next(5, 8));  
+				SimpleMethod(random);
 			}
+
 			Console.ReadLine();
+		}
+		private static void SimpleMethod(Random random)  // 3. Create private static method
+		{
+			Console.WriteLine(random.Next());
 		}
 	}
 }
 
-/*OUTPUT FIRST ATTEMPT( 10 whole numbers), less than 8 including 5 with passing in a seed number of 1
-5
-5
-6
-7
-6
-6
-6
-7
-5
-6
+/*OUTPUT FIRST ATTEMPT( 10 whole numbers)
+200294297
+9625400
+180075971
+2093405899
+1050823125
+218307568
+842187541
+1731040482
+1093104048
+252448969
  
-/*SAME OUTPUT FIRST ATTEMPT( 10 whole numbers), less than 8 including 5 with passing in a seed number of 1
-5
-5
-6
-7
-6
-6
-6
-7
-5
-6
+/*OUTPUT SECOND ATTEMPT( 10 whole numbers)
+1639188983
+1059563538
+544986068
+751871878
+1311898542
+498438722
+304793607
+952429039
+718958311
+173445454
  */
